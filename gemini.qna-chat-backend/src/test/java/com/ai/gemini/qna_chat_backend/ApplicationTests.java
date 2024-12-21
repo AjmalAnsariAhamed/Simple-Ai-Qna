2280103ed7213f8f2e8e2c8137d66754c833c8b6
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2024-12-21 11:51:34 +0530

    adding-other-Files-which-forgot-to-add

---
+++ gemini.qna-chat-backend/src/test/java/com/ai/gemini/qna_chat_backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.gemini.qna_chat_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
