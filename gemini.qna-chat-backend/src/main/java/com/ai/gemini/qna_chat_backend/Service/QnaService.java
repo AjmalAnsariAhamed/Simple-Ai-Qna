35b9be41a351b1b9134bb054f0df6384448b0067
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2024-12-21 11:50:18 +0530

    added-the-QnaService-class

---
+++ gemini.qna-chat-backend/src/main/java/com/ai/gemini/qna_chat_backend/Service/QnaService.java
@@ -0,0 +1,15 @@
+package com.ai.gemini.qna_chat_backend.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QnaService {
+    //Acess to ApiKey and url (Gemini)
+
+    public String getAnswer(String question){
+        //Construct the request payload
+        //make Api call
+        //return response
+        return "helo";
+    }
+}
