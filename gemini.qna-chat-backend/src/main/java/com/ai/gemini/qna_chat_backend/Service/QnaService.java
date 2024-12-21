94211c81cd0b3aaffcd29cf7c28ea5e59556ddfb
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2024-12-21 22:35:00 +0530

    completed-QnaServices-service

--- gemini.qna-chat-backend/src/main/java/com/ai/gemini/qna_chat_backend/Service/QnaService.java
+++ gemini.qna-chat-backend/src/main/java/com/ai/gemini/qna_chat_backend/Service/QnaService.java
@@ -2,6 +2,9 @@ package com.ai.gemini.qna_chat_backend.Service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @Service
 public class QnaService {
@@ -11,10 +14,31 @@ public class QnaService {
     @Value("${gemini.api.key}")
     private String geminiApiKey;
 
+
+    private final WebClient webClient;
+
+    public QnaService(WebClient.Builder webClient) {
+        this.webClient = webClient.build();
+    }
+
     public String getAnswer(String question){
         //Construct the request payload
+        Map<String, Object> requestBody= Map.of("contents",new Object[]{
+                Map.of("parts",new Object[]{
+                        Map.of("text", question)
+                })
+        });
+        //{content:[{parts:[{text:Qery}]}]} this the format that
+        //gemini api will accept
+
         //make Api call
+        String response= webClient.post().uri(geminiApiUrl+geminiApiKey)
+                .header("Content-Type","application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
         //return response
-        return "helo";
+        return response;
     }
 }
