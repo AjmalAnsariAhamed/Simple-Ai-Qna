4ecc218241e86b8592605f7a1afb43e9601eca16
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2024-12-21 12:01:04 +0530

    SETTING-THE-URL&KEY

--- gemini.qna-chat-backend/src/main/java/com/ai/gemini/qna_chat_backend/Service/QnaService.java
+++ gemini.qna-chat-backend/src/main/java/com/ai/gemini/qna_chat_backend/Service/QnaService.java
@@ -1,10 +1,15 @@
 package com.ai.gemini.qna_chat_backend.Service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QnaService {
     //Acess to ApiKey and url (Gemini)
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
 
     public String getAnswer(String question){
         //Construct the request payload
