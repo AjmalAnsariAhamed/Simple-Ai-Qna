503f7ebd6e48e7b93691947b0745260ff6383bbd
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2024-12-21 20:11:15 +0530

    added-ai-conroller-with-normal-paamConstructor, lombok's AllArgsConstructor is not working

---
+++ gemini.qna-chat-backend/src/main/java/com/ai/gemini/qna_chat_backend/controller/AIController.java
@@ -0,0 +1,26 @@
+package com.ai.gemini.qna_chat_backend.controller;
+
+import com.ai.gemini.qna_chat_backend.Service.QnaService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+//@AllArgsConstructor
+@RequestMapping("/ask/qna")
+public class AIController {
+    @Autowired
+    private  QnaService service;
+    @GetMapping
+    public ResponseEntity<String> askQuestion(@RequestBody Map<String, String> payload) {
+        String question = payload.get("question");
+        String answer = service.getAnswer(question);
+        return ResponseEntity.ok(answer);
+    }
+}
