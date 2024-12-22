825b3d7a2235b9c1813badfd6c039575bedec03d
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2024-12-22 10:58:24 +0530

    tailwind added

--- gemini.qna-chat-backend/src/main/java/com/ai/gemini/qna_chat_backend/controller/AIController.java
+++ gemini.qna-chat-backend/src/main/java/com/ai/gemini/qna_chat_backend/controller/AIController.java
@@ -2,22 +2,18 @@ package com.ai.gemini.qna_chat_backend.controller;
 
 import com.ai.gemini.qna_chat_backend.Service.QnaService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/api/qna/ask")
+@RequestMapping("/api/qna")
 public class AIController {
 
     private final  QnaService service;
-    @GetMapping
+    @PostMapping("/ask")
     public ResponseEntity<String> askQuestion(@RequestBody Map<String, String> payload) {
         String question = payload.get("question");
         String answer = service.getAnswer(question);
